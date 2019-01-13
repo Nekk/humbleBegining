c591f6afc4262ba84d707c87e589b695466880fd
Piyabuht Wong <piyabuhtwong@Piyabuhts-MacBook-Pro.local>
2019-01-14 00:57:28 +0700

    try to implement counter as a component && add open browser when run server.

---
+++ src/components/counter.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="Counter">
+    Counter: {{count}}
+    <button @click="count += 1">Increment</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name:'Counter',
+    data(){
+      return{
+        count: 1
+      }
+    }
+  }
+</script>
