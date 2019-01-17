bedf10c7d873b6b0b7d4a6f5dce775ffe6c0acf7
Piyabuht Wong <piyabuhtwong@Piyabuhts-MacBook-Pro.local>
2019-01-18 03:05:01 +0700

    add Vuetify into the project && test passed

--- src/components/counter.vue
+++ src/components/counter.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="Counter">
-    Counter: {{count}}
-    <button @click="count += 10">Increment</button>
+    <v-app id="someID">
+      <div>
+        <h1>This is a countesssxsr</h1>
+        Counter: {{count}}
+        <v-btn color="success" @click="count += 10">Increment</v-btn>
+      </div>
+    </v-app>
   </div>
 </template>
 
